564719c9ff0a8632641ab6bea2823aa2b664b7a1
mohammad sadeghi hardangi <mohammad>
2021-04-17 21:41:29 +0430

    2nd and 3rd programs raedy to go

---
+++ 1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int int main() {
+  /* code */
+  cout<<"this is my second program"
+  return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int int main() {
+  /* code */
+  cout<<"this is my third program"
+  return 0;
+}
