bbef4a43f5e72553ba0c122e977de7cbac6c4842
mohammad sadeghi hardangi <mohammad>
2021-04-20 16:13:06 +0430

    yep

--- index.cpp
+++ index.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-using namespace std; int main() {
+
+using namespace std;
+class player{
+private:
+  int health;
+public:
+  int set_health(int n);
+  void show(bool);
+};
+int player::set_health(int n){
+  health=n;
+}
+void player::show(bool){
+  cout<<health;
+}
+int main() {
   /* code */
   cout << "hello there my dear" << '\n';
+  int y;
+  cin>>y;
+  {player ali;
+  ali.set_health(y);
+  ali.show(1);}
 
   return 0;
 }
